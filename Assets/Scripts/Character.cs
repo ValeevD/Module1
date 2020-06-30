aeb011cc7270d49df8cb07240aaaf2f1467acd7d
ValeevDA <valeevdmitriy@gmail.com>
2020-06-30 21:57:22 +1000

    removed beginning attack/attack dead enemy

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -55,7 +55,7 @@ public class Character : MonoBehaviour
     void AttackEnemy()
     {
         //HW1+++
-        if(IsDead())
+        if(IsDead() || enemy.IsDead())
             return;
         //HW1---
         
